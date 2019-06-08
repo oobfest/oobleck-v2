cc57f3ad6b6514c324586f732e6e6fe92b242195
Tyler Lane <sld.potato@gmail.com>
2019-06-08 00:54:24 -0500

    Moved some files around

---
+++ website/static/js/calendar/app.js
@@ -0,0 +1,126 @@
+// Todo: https://codepen.io/alenabdula/pen/ayyQOv?editors=1010
+let app = new Vue({
+  el: "#app",
+  store: store,
+  data() {
+    return {
+      days: [
+        {day: "Tuesday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "Fallout"}
+        ]},
+        {day: "Wednesday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "Fallout"},
+        ]},
+        {day: "Thursday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "ColdTowne"},
+          {name: "Fallout"},
+          {name: "Velveeta"},
+        ]},
+        {day: "Friday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "ColdTowne"},
+          {name: "Fallout"},
+          {name: "Velveeta"},
+        ]},
+        {day: "Saturday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "Hideout Classroom"},
+          {name: "ColdTowne"},
+          {name: "Velveeta"}
+        ]},
+        {day: "Sunday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+          {name: "Hideout Classroom"},
+          {name: "Fallout"},
+          {name: "Velveeta"},
+          {name: "North Door"}
+        ]},
+        {day: "Monday",
+        venues: [
+          {name: "Hideout Down"},
+          {name: "Hideout Up"},
+        ]}
+      ],
+      editMode: true,
+      hideNavigation: false
+    }
+  },
+  methods: {
+    toggleNavigation() {
+      if(this.hideNavigation) {
+        document.getElementById("dash-nav").style.display = 'flex';
+        document.getElementsByTagName("main")[0].style["margin-left"] = '206px'
+        document.getElementsByClassName('dash-toolbar')[0].style.left = '206px'
+      }
+      else {
+        document.getElementById("dash-nav").style.display = 'none';
+        document.getElementsByTagName("main")[0].style["margin-left"] = 0
+        document.getElementsByClassName('dash-toolbar')[0].style.left = 0
+      }
+      this.hideNavigation = !this.hideNavigation
+    },
+  },
+
+  created() {
+    let self = this
+
+    // Get Shows
+    axios
+      .get('/api/shows')
+      .then(function(response) {
+        self.$store.commit('setupShows', response.data)
+      })
+      .catch(function(error) {
+        alert("Error getting shows :(")
+        console.log(error)
+      })
+
+    // Get Acts
+    axios
+      .get('/api/acts')
+      .then(function(response) {
+        self.$store.commit('setupActs', response.data)
+      })
+      .catch(function(error) {
+        alert("Error getting acts :(")
+        console.log(error)
+      })
+
+    // Get Workshops
+    axios
+      .get('/api/workshops/')
+      .then(function(response) {
+        self.$store.commit('setupWorkshops', response.data)
+      })
+      .catch(function(error) {
+        alert("Error getting workshops :(")
+        console.log(error)
+      })
+
+    // Get Hosts
+    axios
+      .get('/api/hosts/')
+      .then(function(response) {
+        self.$store.commit('setupHosts', response.data)
+      })
+      .catch(function(error){
+        alert("Error getting hosts :(")
+        console.log(error)
+      })
+  }
+})
