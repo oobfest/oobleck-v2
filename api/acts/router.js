c9e01cf0bf1461b4a7afda1c645eed8c4fc7725d
Tyler Lane <sld.potato@gmail.com>
2019-07-08 22:15:46 -0500

    Make email validation public

--- api/acts/router.js
+++ api/acts/router.js
@@ -4,17 +4,15 @@ let isLoggedIn = require('../is-logged-in')
 
 let router = express.Router()
 
-router.use(isLoggedIn)
-
 router.route('/')
-  .post(controller.create)
-  .get(controller.read)
+  .post(isLoggedIn, controller.create)
+  .get(isLoggedIn, controller.read)
 
 router.route('/validate-performer-email/')
   .post(controller.validatePerformerEmail)
 
 router.route('/:id')
-  .get(controller.read)
-  .put(controller.update)
+  .get(isLoggedIn, controller.read)
+  .put(isLoggedIn, controller.update)
 
 module.exports = router
