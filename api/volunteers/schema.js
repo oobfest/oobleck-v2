7de63802cc4130bba9d885044bbc1f85f763f0d3
Tyler Lane <sld.potato@gmail.com>
2019-07-28 16:58:09 -0500

    Add volunteer form, handle free shows

---
+++ api/volunteers/schema.js
@@ -0,0 +1,28 @@
+let mongoose = require('mongoose')
+
+module.exports = mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  type: String,
+  referral: String,
+  volunteeredBefore: Boolean,
+  performing: Boolean,
+  hours: Number,
+  availability: [String],
+  attendingOrientation: Boolean,
+  notes: String,
+
+  // Driver
+  car: {
+    carType: String,
+    passengerCount: Number,
+    licensePlateNumber: String
+  },
+
+  // Tech
+  tech: {
+    previousExperience: String,
+    hasTechedOobBefore: Boolean
+  }
+})
